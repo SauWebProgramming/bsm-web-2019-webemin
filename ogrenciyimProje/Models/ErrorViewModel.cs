3698c301fc1b4b87831aa2a57e8a6e5a1128a230
webemin <eminmuhammed1999@gmail.com>
2019-12-06 11:29:52 +0300

    ilk gönderi

---
+++ ogrenciyimProje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ogrenciyimProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
