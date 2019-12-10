cfb549509ce819e17f535ca7f8f0846f1591b351
webemin <eminmuhammed1999@gmail.com>
2019-12-10 17:35:42 +0300

    Layout değişimi

---
+++ ogrenciyimProje/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ogrenciyimProje.Data;
+
+[assembly: HostingStartup(typeof(ogrenciyimProje.Areas.Identity.IdentityHostingStartup))]
+namespace ogrenciyimProje.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
